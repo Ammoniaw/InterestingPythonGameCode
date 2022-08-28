9def8b640ac317b2bb1850037589fe6a5be2b280
Ammoniaw <73943699+Ammoniaw@users.noreply.github.com>
2022-08-29 03:25:43 +0800

    ball
    This reverts commit 6d8ada1e92366b0fcbcebcb997460ab0bc2f9e22.

--- Unit02-ball/2-11 弹跳的运动轨迹.py
+++ Unit02-ball/2-11 弹跳的运动轨迹.py
@@ -13,7 +13,9 @@ speed_y = 6
 speed_x = 5
 
 # 小球的半径r
-r = 5 # 缩小小球的半径
+r = 5  # 缩小小球的半径
+
+
 def draw():
     # 不绘制背景
     # screen.fill('white')
@@ -21,13 +23,16 @@ def draw():
     screen.draw.filled_circle((x, y), r, 'blue')
 
 # 添加更新代码
+
+
 def update():
-    global y, speed_y, x, speed_x # 使用global语句对变量y,speed_y进行全局修改
+    global y, speed_y, x, speed_x  # 使用global语句对变量y,speed_y进行全局修改
     y += speed_y
     x += speed_x
-    if y >= HEIGHT - r or y <= r: # 小球碰触到最底部时或者碰触到最顶部时
+    if y >= HEIGHT - r or y <= r:  # 小球碰触到最底部时或者碰触到最顶部时
         speed_y = -speed_y
     if x >= WIDTH - r or x <= r:  # 小球碰到左右边界时回弹
         speed_x = -speed_x
+
 
 pgzrun.go()
